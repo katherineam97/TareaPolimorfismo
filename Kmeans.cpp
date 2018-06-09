7570cdd5ee84500758a4e89b64402f04ea6f76c6
katherineam97 <39163032+katherineam97@users.noreply.github.com>
2018-06-08 18:15:49 -0600

    Add files via upload

--- Kmeans.cpp
+++ Kmeans.cpp
@@ -16,15 +16,17 @@ int Kmeans::tamanoLista(Lista * lista){
 	for(Lista::Iterator i = lista->begin(); i != lista->end(); ++i){
 		++tamano;
 	}
+	tamanoL=tamano;
 	return tamano;
 }
 /**Metodo que recorre la lista y va borrando todo lo que hay en ella, "la vacia"
  @param lista
 */
 void Kmeans::reiniciarLista(Lista & lista){
-   for(Lista::Iterator i = lista.begin(); i != lista.end(); ++i){
+	Lista::Iterator i = lista.begin();
+   while(i != lista.end()){
 	    lista.borrar(i);
-	}	
+	}
 }
 
 
@@ -80,14 +82,27 @@ int Kmeans::distanciaMenor(double distancia1, double distancia2){
  @param lista
  @return lista
 */
+int Kmeans::buscarMenor(double * vector, int tamano){
+	int pos;
+    double cero=0.0;
+	double menor= vector[1];
+	for(int a=0; a<tamano;++a){//busqueda del menor para centroide 1
+		if(vector[a]<= menor && vector[a]!= cero){
+			pos=a;
+			menor= vector[a];
+		}	
+	}
+    return pos;
+}
+
 Lista * Kmeans::agrupar( Lista * lista){
-	
+	int pos1=0;
+	int pos2=0;
 	
 	if ( ++(lista->begin()) != NULL){//si hay mas de un elementoen lista
 	//se escogen los dos primeros centroides de manera aleatoria
 	Lista::Iterator cent1=lista->begin();
 	Lista::Iterator cent2= ++(lista->begin());
-	
 	double ** matriz= matrizDistancias(lista,cent1 ,cent2 ); 
 	Lista * grupo1 = new Lista();//representa los elementos agrupados con el centroide1
 	Lista * grupo2 = new Lista();//representa los elementos agrupados con el centroide2
@@ -104,9 +119,13 @@ Lista * Kmeans::agrupar( Lista * lista){
 		}
 
 	}
-        
+
+
 	for(int n=0; n<50; ++n){//se puso como condicion de parada un numero definido de iteraciones (50)
 	//empieza la busqueda de los centroides
+    Lista::Iterator * cent;
+	Lista::Iterator * centr;
+    
 	double * vector1= new double[tamanoLista(grupo1)];// se inicializan vectores para encontrar la posicion del mas cercano al centroide anterior
 	double * vector2= new double[tamanoLista(grupo2)];
 	int i=0;
@@ -124,35 +143,26 @@ Lista * Kmeans::agrupar( Lista * lista){
 
 	}
 	
-	int pos1=0;
-	int pos2=0;
-	double menor= vector1[0];
-	for(int a=0; a<tamanoLista(grupo1);++a){//busqueda del menor para centroide 1
-		if(vector1[a]< menor){
-			pos1=a;
-			menor= vector1[a];
-		}	
-	}
-	
-	menor=vector2[0];
-	for(int a=0; a<tamanoLista(grupo2);++a){//busqueda del menor centroide 2
-		if(vector2[a]< menor){
-			pos2=a;
-			menor= vector2[a];
-		}	
-	}
+    
+	pos1= matrizMenor(grupo1);
+	pos2= matrizMenor(grupo2);
 
 	delete []vector1;
 	delete []vector2;
-    vector1=0;
+	vector1=0;
 	vector2=0;
-	
-    matriz= matrizDistancias(lista, centroide(grupo1,pos1) , centroide(grupo2,pos2) );
-    
-    reiniciarLista(*grupo1);
-    reiniciarLista(*grupo2);
-	
+	cent = centroide(grupo1,pos1);
+	centr = centroide(grupo2,pos2);
+    matriz= matrizDistancias(lista,cent1  ,cent2  );
+    reiniciarLista(*grupo1);//representa los elementos agrupados con el centroide1
+	reiniciarLista(*grupo2);//representa los elementos agrupados con el centroide2
+
+	delete cent;
+	delete centr;
+	cent=0;
+	centr=0;
 	it= lista->begin();
+    
     for(int x=0; x <this->tamanoL && it!= lista->end() ; ++it, ++x){
 		if(distanciaMenor(matriz[x][0], matriz[x][1])==0){
 			*grupo1 +=  (*it);
@@ -162,13 +172,15 @@ Lista * Kmeans::agrupar( Lista * lista){
 		}
 
 	}
+   
         } //terminan las 50 iteraciones
 
 	*grupos += grupo1;
 	*grupos += grupo2;
 	grupo1->~Lista();
 	grupo2->~Lista();
-
+    grupo1=0;
+    grupo2=0;
 	return grupos;
 	
 	}else{//si solo hay un elemento en lista se retorna a ella misma
@@ -184,15 +196,51 @@ Lista * Kmeans::agrupar( Lista * lista){
  @param lista, pos
  @return iterador (nuevo)
 */
-Lista::Iterator Kmeans::centroide(Lista * lista, int pos){
-	Lista::Iterator nuevo; 
+Lista::Iterator * Kmeans::centroide(Lista * lista, int pos){
+	Lista::Iterator * nuevo; 
 	Lista::Iterator i=lista->begin();
-	for(int a=0; a<pos && i!= lista->end() ;++i){
-		nuevo=i;
+	for(int a=0; a<=pos && i!= lista->end() ;++i, ++a){
+		if(a==pos){
+			nuevo = new Lista::Iterator(i);
+		}
 	}
-
 	return nuevo;
 
 }
 
+int  Kmeans::matrizMenor(Lista * lista){
+	this->tamanoL=tamanoLista(lista);
+   double * vector= new double[tamanoL];
+   double ** matriz = new double * [tamanoL];
+   
+   for(int i=0; i < tamanoL ;++i){
+   		matriz[i]= new double[tamanoL];
+     }
+     int f=0;
+     int c=0;
+    
+    for(Lista::Iterator i=lista->begin(); i!=lista->end();++i){
+        for(Lista::Iterator j=lista->begin(); j!=lista->end();++j){
+           matriz[f][c]= (*i)->distancia(*j);
+           ++c;
+        }
+        c=0;
+        ++f;
+    }
 
+    double suma=0.0;
+    int a=0;
+
+    for(int i=0; i<tamanoL;++i){
+
+    	for(int j =0; j<tamanoL;++j){
+              suma+=matriz[i][j];
+    	}
+    	vector[a]=suma;
+    	suma=0.0;
+    	++a;
+    }
+
+    return buscarMenor(vector, tamanoL);
+
+}
--- Kmeans.h
+++ Kmeans.h
@@ -11,9 +11,11 @@ class Kmeans: public Agrupador{
 		Kmeans();//constructor por defecto
         double ** matrizDistancias(Lista *,Lista::Iterator, Lista::Iterator);// calcula la distancia que existe entre los centroides y los otros elementos
         int distanciaMenor(double, double); // busca en la matriz la menor distancia  
-        Lista::Iterator centroide(Lista *, int);//busca centroides despues de la primera busqueda(que es al azar)
+        Lista::Iterator * centroide(Lista *, int);//busca centroides despues de la primera busqueda(que es al azar)
         Lista * agrupar(Lista *);//metodo final que devuelve la lista de los grupos realizados
 		int tamanoLista(Lista *);//calcula el tamano de la lista
-        void reiniciarLista(Lista &); // vacia una lista que recibe por referencia              
+        void reiniciarLista(Lista &); // vacia una lista que recibe por referencia
+        int buscarMenor(double *, int);  
+        int matrizMenor(Lista *);// para en contrar el centroide 
 };
 #endif
